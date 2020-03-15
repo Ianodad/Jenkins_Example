35bfcc094a1bd38987573b148c576937b575e4ca
ianodad <ianodad@gmail.com>
2020-03-15 16:35:14 +0300

    pulled

---
+++ nodejsdocker.groovy
@@ -0,0 +1,25 @@
+ob('NodeJs Example') {
+	scm{
+		git('git://https://github.com/Ianodad/Mailer.git') node -> {
+			node / gitConfigName('DSL User)
+			node / gitConfigEmail('jenkins-dsl@newtech.academy')
+		}
+	}
+	triggers {
+		scm('H/5 * * * *')	
+	}
+	wrappers {
+		nodejs('nodejs')	
+	}
+	steps {
+		dockerBuildAndPublish {
+			repositoryName('ianodad/mailer')
+			tag('${GIT_REVISION, length=9}')
+			registryCredentials('dockerhub')
+			forcePull(false)
+			forceTag(false)
+			createFingerprints(false)
+			skipDecorate()		
+		}	
+	}
+}
